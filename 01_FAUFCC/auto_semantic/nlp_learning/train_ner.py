cb9c5010cd228ef9956eeb57d3dfdce897b08491
flairmix <d9067246810@gmail.com>
2025-03-25 18:13:04 +0300

    wip

--- 01_FAUFCC/auto_semantic/nlp_learning/train_ner.py
+++ 01_FAUFCC/auto_semantic/nlp_learning/train_ner.py
@@ -4,7 +4,7 @@ from spacy.scorer import Scorer
 import json
 
 # Загрузка модели
-nlp = spacy.load("custom_ner_model_ru")
+nlp = spacy.load("ru_core_news_sm")
 ner = nlp.get_pipe("ner")
 
 # Добавляем кастомные лейблы
@@ -34,30 +34,29 @@ for item in test_data_json:
     entities = [(ent['start'], ent['end'], ent['label']) for ent in item['entities']]
     test_data.append((item['text'], {"entities": entities}))
 
-# # Обучение модели
-# optimizer = nlp.resume_training()
-# for epoch in range(30):
-#     losses = {}
-#     for text, ann in train_data:
-#         example = Example.from_dict(nlp.make_doc(text), ann)
-#         nlp.update([example], drop=0.25, losses=losses)
-#     print(f"Epoch {epoch+1} - Loss: {losses['ner']:.4f}")
+# Обучение модели
+optimizer = nlp.resume_training()
+for epoch in range(5):
+    losses = {}
+    for text, ann in train_data:
+        example = Example.from_dict(nlp.make_doc(text), ann)
+        nlp.update([example], drop=0.25, losses=losses)
+    print(f"Epoch {epoch+1} - Loss: {losses['ner']:.4f}")
 
-# # Сохранение обученной модели
-# nlp.to_disk("custom_ner_model_ru")
-# print("✅ Модель сохранена в 'custom_ner_model_ru'")
+# Сохранение обученной модели
+nlp.to_disk("01_FAUFCC\\auto_semantic\\nlp_learning\\custom_ner_model_ru")
 
-examples = []
-for item in test_data_json:
-    doc = nlp.make_doc(item['text'])
-    entities = [(ent['start'], ent['end'], ent['label']) for ent in item['entities']]
-    examples.append(Example.from_dict(doc, {"entities": entities}))
+print("✅ Модель сохранена в 'custom_ner_model_ru'")
 
-# Оценка на тестовом наборе
+# Оценка на тестовой выборке
 scorer = Scorer()
-scores = scorer.score(examples)
+examples = []
+for text, ann in test_data:
+    doc = nlp.make_doc(text)
+    examples.append(Example.from_dict(doc, ann))
 
+results = scorer.score_spans(examples, "ents")
 print("\n--- Evaluation on test set ---")
-print(f"Precision: {scores['ents_p']:.2f}%")
-print(f"Recall:    {scores['ents_r']:.2f}%")
-print(f"F1-score:  {scores['ents_f']:.2f}%")
+print(f"Precision: {results['ents_p']:.2f}%")
+print(f"Recall:    {results['ents_r']:.2f}%")
+print(f"F1-score:  {results['ents_f']:.2f}%")
